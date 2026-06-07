f8f2ac8aecc8175a896df2aec8c946bbd3d11b56
pierreduplessis6912-gif <pierreduplessis6912@gmail.com>
2026-06-07 21:36:08 +0200

    feat: inject counter JS

--- build-worker/src/archetypes/results.js
+++ build-worker/src/archetypes/results.js
@@ -896,6 +896,16 @@ document.querySelectorAll('a[href^="#"]').forEach(a=>{
     if(t){e.preventDefault();t.scrollIntoView({behavior:'smooth',block:'start'})}
   });
 });
+
+// Counters
+(function(){
+  var s='${client.slug}';
+  if(!s)return;
+  new Image().src='/'+s+'/ping';
+  document.querySelectorAll('a[href*="wa.me"]').forEach(function(a){
+    a.addEventListener('click',function(){new Image().src='/'+s+'/wa';},{once:true,passive:true});
+  });
+})();
 </script>
 </body>
 </html>`;
